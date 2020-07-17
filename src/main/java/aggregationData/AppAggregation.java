0b1854761bcf987f313ff369510c26142062e27b
Rustymattok <mrArxi@gmail.com>
2020-07-17 12:52:14 +0300

    add Readme file

--- src/main/java/aggregationData/AppAggregation.java
+++ src/main/java/aggregationData/AppAggregation.java
@@ -1,23 +1,21 @@
 package aggregationData;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.*;
 
 /**
  * Application for pars url and sorted info to other file.
  */
 public class AppAggregation {
     public static void main(String[] args) {
+        ExecutorService exec = Executors.newFixedThreadPool(3);
         BlockingQueue<ParsInfo> queue = new PriorityBlockingQueue<>();
         BlockingQueue<String> queueSave = new PriorityBlockingQueue<>();
         JsonParsThread jsonParsThread = new JsonParsThread(queue);
         JsonAggregationThread jsonAggregationThread = new JsonAggregationThread(queue,queueSave,jsonParsThread);
         FileInputThread fileInputThread = new FileInputThread(queueSave);
-        Thread threadPut = new Thread(jsonParsThread);
-        Thread threadPars = new Thread(jsonAggregationThread);
-        Thread threadSave = new Thread(fileInputThread);
-        threadPars.start();
-        threadSave.start();
-        threadPut.start();
+        exec.submit(jsonParsThread);
+        exec.submit(jsonAggregationThread);
+        exec.submit(fileInputThread);
+        exec.shutdown();
     }
 }
